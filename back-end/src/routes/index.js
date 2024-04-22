8c138d66cb38910b5d2e9352032fe23e8114f1af
Santiagos-Vertigo <dgo.narvaez@gmail.com>
2024-04-22 04:28:48 -0500

    Update index.js

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,10 +2,12 @@ import { logInRoute } from './logInRoute';
 import { signUpRoute } from './signUpRoute';
 import { testRoute } from './testRoute';
 import { updateUserInfoRoute } from './updateUserInfoRoute';
+import { findMatchRoute } from './findMatchRoute'; // Import the findMatchRoute
 
 export const routes = [
     logInRoute,
     signUpRoute,
     testRoute,
     updateUserInfoRoute,
+    findMatchRoute, // Add the findMatchRoute to the routes array
 ];
